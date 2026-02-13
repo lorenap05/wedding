528e6c1340c2c0e7a99708d4cc04518e3bb3853c
lorenap05 <lleite@chicagobooth.edu>
2026-02-13 21:29:12 +0000

    admin pages layout

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,9 @@ module Wedding
     config.load_defaults 7.1
     config.time_zone = "America/Sao_Paulo"
     config.i18n.default_locale = :'pt-BR'
+  
+ # Needed for tunneled dev hosts (e.g. github.dev) where browser Origin can be localhost
+  # while base_url is the forwarded public domain.
+  config.action_controller.forgery_protection_origin_check = false
   end
 end
