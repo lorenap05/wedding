be05fcc49abfaef526200638452448cc7a858da7
lorenap05 <lleite@chicagobooth.edu>
2026-02-14 00:33:48 +0000

    fix bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def safe_image_tag(source, fallback: nil, **options)
+    image_tag(source, **options)
+  rescue StandardError
+    return "".html_safe if fallback.blank?
+
+    image_tag(fallback, **options)
+  end
 end
