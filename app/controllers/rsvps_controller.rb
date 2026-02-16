ca71b508c144ce5c6a8029c2408f46c4edc942fc
lorenap05 <lleite@chicagobooth.edu>
2026-02-16 04:12:49 +0000

    update rsvp thank you pop up

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -8,8 +8,8 @@
      @rsvp = Rsvp.new(rsvp_params)
  
      if @rsvp.save
-+      send_rsvp_notification(@rsvp)
-       redirect_to new_rsvp_path, notice: "Confirmação enviada com sucesso!"
+      send_rsvp_notification(@rsvp)
+      redirect_to new_rsvp_path(show_thanks: "1"), confirmation: "Confirmação enviada com sucesso!"
      else
        render :new, status: :unprocessable_entity
      end
@@ -20,13 +20,13 @@
    def rsvp_params
      params.require(:rsvp).permit(:name, :attending)
    end
-+
-+  def send_rsvp_notification(rsvp)
-+    recipient_email = AdminUser.order(:id).pick(:email) || ENV["ADMIN_EMAIL"]
-+    return if recipient_email.blank?
-+
-+    RsvpMailer.with(rsvp: rsvp, recipient_email: recipient_email).notification.deliver_now
-+  rescue StandardError => e
-+    Rails.logger.error("[RSVP_NOTIFICATION] Failed to send email for RSVP ##{rsvp.id}: #{e.class} - #{e.message}")
-+  end
- end
+
+  def send_rsvp_notification(rsvp)
+    recipient_email = AdminUser.order(:id).pick(:email) || ENV["ADMIN_EMAIL"]
+    return if recipient_email.blank?
+
+    RsvpMailer.with(rsvp: rsvp, recipient_email: recipient_email).notification.deliver_now
+  rescue StandardError => e
+    Rails.logger.error("[RSVP_NOTIFICATION] Failed to send email for RSVP ##{rsvp.id}: #{e.class} - #{e.message}")
+  end
+end 
